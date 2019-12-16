7c5e9d0700baddd628207f9c8bc2466aa720f959
Daniel Kaniowski <55710615+danielkaniowski@users.noreply.github.com>
2019-12-16 15:00:39 +0000

    Add files via upload

---
+++ app_dictionary.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open('data.json'))
+
+def translate_word(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input('Did you mean %s instead? Please type yes or no: ' % get_close_matches(w, data.keys())[0])
+        if yn == 'yes':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == 'no':
+            return'This word is not valid. Please try again'
+        else:
+            return'I did not understand your entry'
+    else:
+        return'This word is not valid.  Please try again.'
+
+word = input('Please Enter a Word: ')
+output = translate_word(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
+
+print(translate_word(word))
+
